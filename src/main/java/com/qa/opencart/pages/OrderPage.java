7d54649eb033c67b5239788af1125333462dcce7
MeenaKanda <meenakanda2012@gmail.com>
2023-03-10 20:44:47 -0800

    adder orderpage

---
+++ src/main/java/com/qa/opencart/pages/OrderPage.java
@@ -0,0 +1,13 @@
+package com.qa.opencart.pages;
+
+import org.openqa.selenium.By;
+
+public class OrderPage {
+	
+	By loc = By.id("order");
+
+	public static void main(String[] args) {
+		System.out.println("get order");
+	}
+
+}
