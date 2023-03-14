3926fcd067f174dc5545b808644051921ff88263
MeenaKanda <meenakanda2012@gmail.com>
2023-03-13 18:10:17 -0700

    Mail is added finally after conflict with meena

---
+++ src/main/java/com/qa/opencart/pages/Mail.java
@@ -0,0 +1,15 @@
+package com.qa.opencart.pages;
+
+public class Mail {
+
+
+	public void sendMail() {
+		System.out.println("send mail - MeenaKanda");
+		System.out.println("mail code is done - MeenaKanda");
+	}
+
+	public void getMail() {
+		System.out.println("get mail - MeenaKanda");
+	}
+
+}
