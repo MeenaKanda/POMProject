9a73567505a33de84b9a37f33815973489e499bf
MeenaKanda <meenakanda2012@gmail.com>
2023-03-13 17:07:03 -0700

    mail MeenaKanda

---
+++ src/main/java/com/qa/opencart/pages/Mail.java
@@ -0,0 +1,13 @@
+package com.qa.opencart.pages;
+
+public class Mail {
+
+	public void sendMail() {
+		System.out.println("send mail - MeenaKanda");
+	}
+
+	public void getMail() {
+		System.out.println("get mail - MeenaKanda");
+	}
+
+}
